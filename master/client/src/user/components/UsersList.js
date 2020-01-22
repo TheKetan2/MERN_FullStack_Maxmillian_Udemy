661546e2019b5d68c40dfb1cb78ddfdd1fe21cfd
Ketan <ketantoo@gmail.com>
2020-01-22 15:02:51 +0530

    added card and avatar components

---
+++ master/client/src/user/components/UsersList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./UsersList.css";
+import UserItem from "./UserItem";
+
+export default function UsersList(props) {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users found...</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <ul className="users-list">
+          {props.items.map(user => (
+            <UserItem
+              key={user.id}
+              id={user.id}
+              image={user.image}
+              name={user.name}
+              placesCount={user.places}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
--- master/client/src/user/pages/Users.js
+++ master/client/src/user/pages/Users.js
@@ -1,5 +1,16 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 export default function Users() {
-  return <div>Users</div>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Ketan Ketantanaaaaa",
+      image:
+        "https://miro.medium.com/fit/c/700/210/1*aK_PcqA4wVfsNAVjPmQbrA.jpeg",
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 }
